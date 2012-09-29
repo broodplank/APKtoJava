45dcbd5a956416883ee9c0e7a06300073ed4f1ca
Florent Guichard <guichardflorent@gmail.com>
2012-09-29 17:07:17 +0200

    Critical fix
    Critical fix for dex2jar batch file!

--- tools/dex2jar.bat
+++ tools/dex2jar.bat
@@ -1,4 +1,5 @@
 @echo off
+cd /d %~dp0
 REM
 REM dex2jar - Tools to work with android .dex and java .class files
 REM Copyright (c) 2009-2012 Panxiaobo
@@ -19,4 +20,4 @@ REM
 set CLASSPATH=
 FOR %%i IN ("%~dp0lib\*.jar") DO CALL "%~dp0setclasspath.bat" %%i
 
-java -Xms512m -Xmx1024m -cp "%CLASSPATH%" "com.googlecode.dex2jar.tools.Dex2jarCmd" %*
+java -Xms512m -cp "%CLASSPATH%" "com.googlecode.dex2jar.tools.Dex2jarCmd" %*
